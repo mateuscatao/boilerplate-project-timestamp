bbad30fe974f92e1ce3efd23257535618544f105
mateuscatao <mateuscatao1311@gmail.com>
2023-11-22 21:53:08 -0300

    solution

--- index.js
+++ index.js
@@ -18,13 +18,32 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// My Solution
+app.get("/api", (req, res) => {
+  let unix = new Date().getTime();
+  let utc = new Date().toUTCString();
+  
+  return res.json({ unix: unix, utc: utc });
+})
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", (req, res) => {
+  let date = req.params.date;
+
+  if (!isNaN(Number(date))) {
+    let unix = new Date(Number(date)).getTime();
+    let utc = new Date(Number(date)).toUTCString();
+    
+    return res.json({ unix: unix, utc: utc });
+  }
+
+  let unix = new Date(date).getTime();
+
+  if(isNaN(unix)) return res.json({ error: "Invalid Date" })
+
+  let utc = new Date(Date.parse(date)).toUTCString();
+
+  return res.json({ unix: unix, utc: utc });
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
